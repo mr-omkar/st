9578734d2dafff607d2eac043ee7de68df5bb317
mr-omkar <65303335+mr-omkar@users.noreply.github.com>
2023-05-03 14:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ from sklearn.preprocessing import MinMaxScaler
 from jugaad_data.nse import stock_df
 from streamlit_extras.no_default_selectbox import selectbox
 
-from tf.keras.models import Sequential
-from tf.keras.layers import LSTM,Dense
-from tf.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM,Dense
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 all_stocks = pd.read_csv("https://archives.nseindia.com/content/equities/EQUITY_L.csv")
